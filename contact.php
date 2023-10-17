8837c3cc07918d851d4ddc6706e91e1cc5e1991f
Tushar Gautam <138028564+ttgautam309@users.noreply.github.com>
2023-10-17 15:41:56 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,13 +1,14 @@
 <?php
 //get data from form
- $name = $_POST[ 'name'];
+ $fname = $_POST[ 'fname'];
+ $lname= $_POST[ 'lname'];
+ $name=$fname+$lname;
  $email= $_POST[ 'email'];
- $subject= $_POST[ 'subject'];
  $mobile= $_POST[ 'mobile'];
  $message = $_POST[ 'message'];
- $to = "support@teamsuperking.in";
- $xyz = "Team Superking Customer Details";
- $txt = "Name = ". $name ."\r\nEmail = ". $email . "\r\nMobile Number =" .$mobile." \r\nSubject =" .$subject."\r\nMessage =". $message ;
+ $to = "ttgautam309@gmail.com";
+ $xyz = "Shree Make Over Studio Appointment";
+ $txt = "Name = ". $name ."\r\nEmail = ". $email . "\r\nMobile Number =" . $mobile . "\r\nMessage =". $message ;
  $headers = "From: tsk_customer_detail@teamsuperking.com". "\r\n".
   "СС: ragautam2010@gmail.com";
  if($email!=NULL){
